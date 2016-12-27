6ff06228d493400b77bfc6f8049ae029b59c8a6f
Cliff <fabricatedmind@gmail.com>
2016-12-27 18:09:01 +0000

    complete 1.0.1

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 
 
 
-var parseDate = function(date){
+var parseNatural = function(date){
    
    var theDate = new Date(date);
    var naturalDate = theDate.toLocaleDateString('en-US',{month: 'long', day: 'numeric', year: 'numeric'})
@@ -18,12 +18,35 @@ var parseDate = function(date){
   return rtn; 
 };
 
+var parseUnix = function(date){
+    var theDate = new Date(date * 1000);
+    var naturalDate = theDate.toLocaleDateString('en-US',{month: 'long', day: 'numeric', year: 'numeric'});
+    if (naturalDate === "Invalid Date"){
+       naturalDate = null;
+   }
+   var rtn = {
+        unix: date,
+        natural: naturalDate
+    };
+   return rtn;
+}
 //app.use(parseDate);
 
 app.get("/:date", function(req,res){
-     
-     var theDate = parseDate(req.params.date);
+    var theDate = null;
+    
+     if (req.params.date.match(/[0-9]/)){
+    
+         theDate = parseUnix(req.params.date);
+         
+     }
+     else {
+         
+        theDate = parseNatural(req.params.date);
+        
+     }
      res.send(theDate);
+     
 });
 
 app.listen(8080, function(){
